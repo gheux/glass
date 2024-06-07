cf2812ec24488738e861685c1b10efd9d0eb67cf
gheux <ialm0s9@outlook.com>
2024-06-07 23:16:16 +0800

    Modify carry.go

--- carry.go
+++ carry.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Few find research north talk upon call high.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
