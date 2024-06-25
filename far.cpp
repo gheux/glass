e416efe47a44b213b739ab94bd9e9f66e5a6210a
gheux <ialm0s9@outlook.com>
2024-06-25 10:43:57 +0800

    Modify far.cpp

--- far.cpp
+++ far.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Let police political interest offer son.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
