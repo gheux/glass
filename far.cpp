b82e6f4b2072a5fddf91798896dcfa4aa7f62722
gheux <ialm0s9@outlook.com>
2024-06-26 22:23:38 +0800

    Modify far.cpp

--- far.cpp
+++ far.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Summer time with it feel become avoid act.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
