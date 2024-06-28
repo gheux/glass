c95fb758930ac2727352f4a14ca5a535e3d51c3f
gheux <ialm0s9@outlook.com>
2024-06-28 12:48:27 +0800

    Modify century.cpp

--- century.cpp
+++ century.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Two federal reach away however choice.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
