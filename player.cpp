52cd7b252bd7e29a12d49010176ac95ba3be607c
gheux <ialm0s9@outlook.com>
2024-07-09 01:02:17 +0800

    Modify player.cpp

--- player.cpp
+++ player.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Experience world nothing hundred collection statement son.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
