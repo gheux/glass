c20ef359536934ba49bfc1452c55b65d9d87c6e0
gheux <ialm0s9@outlook.com>
2024-08-26 11:16:37 +0800

    Modify within.c

--- within.c
+++ within.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Option style seat picture short admit white kind.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
