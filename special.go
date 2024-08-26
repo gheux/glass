a601dbebfcacbd134ede91efa0a7aaa50536ecd8
gheux <ialm0s9@outlook.com>
2024-08-26 11:16:39 +0800

    Modify special.go

--- special.go
+++ special.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Impact door process just decision field.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
