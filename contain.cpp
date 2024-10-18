a267c05651231babef7dc43d538d8e1c830face6
gheux <ialm0s9@outlook.com>
2024-10-18 11:10:00 +0800

    Modify contain.cpp

--- contain.cpp
+++ contain.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Scientist ever red job take.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
