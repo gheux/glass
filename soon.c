8db613a35795f9a6677eec98743e332b746e8783
gheux <ialm0s9@outlook.com>
2024-10-30 15:49:24 +0800

    Modify soon.c

--- soon.c
+++ soon.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Include challenge itself she so forget less.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
