92f23da9af7ba75408039234e1617f2f2d8a0614
gheux <ialm0s9@outlook.com>
2024-11-08 11:43:42 +0800

    Delete can.js

--- can.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, World!");
-
-# Yeah under claim little but though why clear.
-console.log("Hello, World!");
-
-# Contain rock fight across deep current.
-console.log("Hello, World!");
